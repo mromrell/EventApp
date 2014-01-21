ea264883fca416f0636afb7f0fdf63ad7e308528
Michael Romrell <mromrell@live.com>
2014-01-21 09:28:29 -0700

    Added extended user model, logins not working

--- ro-frontend/public/js/controllers.js
+++ ro-frontend/public/js/controllers.js
@@ -21,3 +21,42 @@ angular.module('roApp.controllers', [])
             $scope.session = SessionService.getSession();
         });
     }]);
+
+//    // I PULLED IN THIS CONTROLLER From another projects, some for the pieces still need to be updated
+//    .controller('LoginController', ['$scope', '$window', 'loginTitle', 'Restangular', 'SessionService', function($scope, $window, loginTitle, Restangular, SessionService) {
+//        $scope.user = {}
+//
+//        $scope.register = function() {
+//            $window.location = '/register';
+//        }
+//
+//        $scope.doLogin = function() {
+//            console.log('Logging in user: ' + $scope.user.email);
+//            var user = {
+//                'email': $scope.user.email,
+//                'password': $scope.user.password
+//            };
+//
+//            Restangular.all('account/login').customPOST(user)
+//                .then(function(data) {
+//                    if (data == 'Unauthorized') {
+//                        $scope.errorMessage = 'Invalid username and/or password';
+//                    } else {
+//                        SessionService.saveUserSession(data);
+//                        $scope.userSession = data;
+//                        $window.location = '/home';
+//                    }
+//                }), function(response) {
+//                    $scope.errorMessage = response;
+//                };
+//        }
+//
+//        $scope.hasError = function (field, validation) {
+//            if (validation) {
+//                return $scope.loginForm[field].$dirty && $scope.loginForm[field].$error[validation];
+//            }
+//            return $scope.loginForm[field].$dirty && $scope.loginForm[field].$invalid;
+//        };
+//
+//        $scope.loginTitle = loginTitle;
+//    }]);
