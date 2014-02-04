768311c4e7aa875b4bbca01a3e8be1ae71849448
Jake Green <jakegreen@gmail.com>
2014-02-03 18:15:05 -0700

    bla

--- backend/apps/public/models.py
+++ backend/apps/public/models.py
@@ -12,39 +12,37 @@ from django.contrib.auth.hashers import make_password, is_password_usable
 
 @receiver(post_save, sender=User)
 def hash_password(sender, instance=None, created=False, **kwargs):
-   ''' Hashes the password given when a User is created or updated '''
+   # Hashes the password given when a User is created or updated
    if not is_password_usable(instance.password):
        instance.password = make_password(instance.password)
        instance.save()
 
+
 @receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
-    ''' Creates a token whenever a User is created '''
+    # Creates a token whenever a User is created
     if created:
         Token.objects.create(user=instance)
 
+
 class AccountInfo(models.Model):
-    birthdate = models.DateField(auto_now_add=True)
+    birth_date = models.DateField(auto_now_add=True)
     phone = models.CharField(max_length=25, blank=True)
     is_cell = models.BooleanField()
-    contact_me_by
-    social_link
 
+    # social_info = models.ForeignKey(Social, null=True)
     address = models.ForeignKey(Address, null=True)
 
 
-
-    account_type = models.CharField(max_length=25, choices=ACCOUNT_TYPE_LIST, blank=True, null=True)
-
-
+# class Social(models.Model):
+#     facebook_url = models.URLField()
 
 
 class Address(models.Model):
-    ''' Model features for an address '''
     ADDRESS_TYPES = (
         ('Sponsor', 'Sponsor'),
-        ('Event'), 'Event'),
-        ('User'), 'User'),
+        ('Event', 'Event'),
+        ('User', 'User'),
     )
 
     street = models.CharField(max_length=200)
@@ -60,8 +58,9 @@ class Address(models.Model):
     class Meta:
         verbose_name_plural = 'Address'
 
+
 class Location(models.Model):
-    ''' Model features for an address '''
+    # Model features for an address
     eventName = models.CharField(max_length=200)
     gpsLat = models.CharField(max_length=200, blank=True, null=True)
     gpsLng = models.CharField(max_length=200, blank=True, null=True)
@@ -87,14 +86,14 @@ class Location(models.Model):
     class Meta:
         verbose_name_plural = 'Location'
 
+
 class Comment(models.Model):
-    ''' Model features for an address '''
+    # Model features for an address
     user = models.ForeignKey(User)
     locationPostID = models.ForeignKey(Location)
     commentText = models.CharField(max_length=900)
     commentDate = models.DateField(default=datetime.now)
     locationRating = models.IntegerField(blank=True, null=True)
-
 
     def __unicode__(self):
         return u'%s, %s, %s' % (self.user, self.locationPostID, self.commentText)
