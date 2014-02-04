e542da2dff107c6d0e346a52b8e52b0c9e614e88
Michael Romrell <mromrell@live.com>
2014-02-04 15:36:28 -0700

    part way through getting the calendar to work, having issues importing the full version of Jquery

--- frontend/public/js/controllers.js
+++ frontend/public/js/controllers.js
@@ -359,6 +359,21 @@ angular.module('roApp.controllers', [])
         $scope.predicate = '-voteCount';
         $scope.predicate = '-datecreated';
 
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+
+        $scope.events = [
+          {title: 'All Day Event',start: new Date(y, m, 1)},
+          {title: 'Long Event',start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
+          {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
+          {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
+          {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
+          {title: 'Click for Google',start: new Date(y, m, 28),end: new Date(y, m, 29),url: 'http://google.com/'}
+        ];
+        $scope.eventSources = [$scope.events]
+
         // allows images to show up on the homepage
         $scope.imagefinder = function () {
             for (var i = 0; i < $scope.locationList.length; i++) {
