31ea673f6a7f47e7c69d942a0366a40070eadf45
m0ngr31 <joeipson@gmail.com>
2014-02-04 19:05:10 -0700

    Added social and votes views

--- backend/apps/public/views.py
+++ backend/apps/public/views.py
@@ -48,11 +48,13 @@ class PhotoList(generics.ListCreateAPIView):
     model = Photo
     serializer_class = PhotoSerializer
 
+
 class PhotoDetail(generics.RetrieveUpdateDestroyAPIView):
     """List all Locations or create a new Location"""
     #permission_classes = (permissions.IsAuthenticated,)
     model = Photo
     serializer_class = PhotoSerializer
+
 
 class LocationList(generics.ListCreateAPIView):
     """List all Locations or create a new Location"""
@@ -60,11 +62,13 @@ class LocationList(generics.ListCreateAPIView):
     model = Location
     serializer_class = LocationSerializer
 
+
 class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
     """List all Locations or create a new Location"""
     #permission_classes = (permissions.IsAuthenticated,)
     model = Location
     serializer_class = LocationSerializer
+
 
 class CommentList(generics.ListCreateAPIView):
     """List all Locations or create a new Location"""
@@ -79,6 +83,7 @@ class CommentList(generics.ListCreateAPIView):
     #     return query_set
 
 # I Added the stuff above --------------------------------------
+
 
 @api_view(('GET',))
 def comments_by_location(request):
@@ -107,12 +112,14 @@ def authenticate(request):
         c['message'] = 'Login failed!'
         return render_to_response('partials/login.tpl.html', c)
 
+
 @api_view(('GET',))
 def obtain_user_from_token(r, token):
    auth = TokenAuthentication()
    response = auth.authenticate_credentials(token)
    user_id = response[0].id
    return Response(user_id)
+
 
 @api_view(('GET',))
 def uploadedimages(request, event_id):
@@ -128,18 +135,136 @@ def uploadedimages(request, event_id):
     response = [photo_url, event_id]
     return Response(response)
 
+
+@api_view(['GET', 'PUT', 'POST', 'DELETE'])
+def votes(request):
+
+    votes_list = None
+    vote = None
+    up = None
+    down = None
+
+    if 'is_up' in request.DATA and request.DATA['is_up'] is not None:
+        up = Vote.objects.get(id=request.POST.get('is_up', request.DATA['is_up']))
+
+    if 'is_down' in request.DATA and request.DATA['is_down'] is not None:
+        down = Vote.objects.get(id=request.POST.get('is_down', request.DATA['is_down']))
+
+    if 'id' in request.DATA and request.DATA['id'] is not None:
+        vote = Vote.objects.get(id=request.POST.get('id', request.DATA['id']))
+
+    if 'user_id' in request.DATA and request.DATA['user_id'] is not None:
+        if 'event_id' in request.DATA and request.DATA['event_id'] is not None:
+            votes_list = Vote.objects.filter(event_id=request.POST.get('event_id', request.DATA['event_id']),
+                                             user_id=request.POST.get('user_id', request.DATA['user_id']))
+
+    else:
+        votes_list = Vote.objects.all()
+
+    if request.method == 'GET':
+        serialize = VoteSerializer(votes_list)
+        return Response(serialize.data)
+
+    elif request.method == 'PUT':
+        vote = Vote.objects.get(id=request.POST.get('id', request.DATA['id']))
+
+        vote.is_up = up
+        vote.is_down = down
+        vote.user_id = request.POST.get('user_id', request.DATA['user_id'])
+        vote.event_id = request.POST.get('event_id', request.DATA['event_id'])
+
+        vote.save()
+
+        return Response(status=status.HTTP_201_CREATED)
+
+    elif request.method == 'POST':
+        event = Location.objects.get(id=request.POST.get('event_id', request.DATA['event_id']))
+        user = User.objects.get(id=request.POST.get('user_id', request.DATA['user_id']))
+
+        new_vote = Vote(is_up=up, is_down=down, event_id=event, user_id=user)
+
+        new_vote.save()
+
+        return Response(status=status.HTTP_201_CREATED)
+
+    else:
+        vote = Vote.objects.get(id=request.POST.get('id', request.DATA['id']))
+        vote.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'PUT', 'POST', 'DELETE'])
+def social_accounts(request):
+
+    social_list = None
+    social = None
+    facebook = None
+    twitter = None
+    google = None
+
+    if 'facebook' in request.DATA and request.DATA['facebook'] is not None:
+        facebook = request.POST.get('facebook', request.DATA['facebook'])
+
+    if 'twitter' in request.DATA and request.DATA['twitter'] is not None:
+        twitter = request.POST.get('twitter', request.DATA['twitter'])
+
+    if 'google' in request.DATA and request.DATA['google'] is not None:
+        google = request.POST.get('twitter', request.DATA['google'])
+
+    if 'id' in request.DATA and request.DATA['id'] is not None:
+        social = Social.objects.get(id=request.POST.get('id', request.DATA['id']))
+
+    if 'user_id' in request.DATA and request.DATA['user_id'] is not None:
+        social_list = Vote.objects.filter(user_id=request.POST.get('user_id', request.DATA['user_id']))
+
+    else:
+        social_list = Social.objects.all()
+
+    if request.method == 'GET':
+        serialize = SocialSerializer(social_list)
+        return Response(serialize.data)
+
+    elif request.method == 'PUT':
+        social = Social.objects.get(id=request.POST.get('id', request.DATA['id']))
+
+        social.facebook_url = facebook
+        social.google_plus = google
+        social.twitter_handle = twitter
+
+        social.save()
+
+        return Response(status=status.HTTP_201_CREATED)
+
+    elif request.method == 'POST':
+        user = User.objects.get(id=request.POST.get('user_id', request.DATA['user_id']))
+
+        new_social = Social(facebook_url=facebook, google_plus=google, twitter_handle=twitter, user_id=user)
+
+        new_social.save()
+
+        return Response(status=status.HTTP_201_CREATED)
+
+    else:
+        social = Social.objects.get(id=request.POST.get('id', request.DATA['id']))
+        social.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 def logout(request):
     auth.logout(request)
     return JSONResponse([{'success': 'Logged out!'}])
 
+
 class NewAuthToken(ObtainAuthToken):
-   def post(self, request):
-       serializer = self.serializer_class(data=request.DATA)
-       if serializer.is_valid():
-           token, created = Token.objects.get_or_create(user=serializer.object['user'])
-           data = {
-               'user': UserSerializer(User.objects.filter(auth_token=token)).data,
-               'token': token.key,
-           }
-           return Response(data)
-       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        serializer = self.serializer_class(data=request.DATA)
+        if serializer.is_valid():
+            token, created = Token.objects.get_or_create(user=serializer.object['user'])
+            data = {
+                'user': UserSerializer(User.objects.filter(auth_token=token)).data,
+                'token': token.key,
+            }
+            return Response(data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
